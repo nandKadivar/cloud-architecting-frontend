ece7ff1b675ee873f1ecedcdde2e4c6740b8c70f
Library User <lib-user@atss-air.home>
2023-11-23 20:38:29 -0400

    changed axios endpoint to network load balancer DNS

--- src/api/customAxiosConfig/CustomAxiosConfig.js
+++ src/api/customAxiosConfig/CustomAxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios instance for making requests
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "backend-nlb-c78efa861b0e6cc0.elb.us-east-1.amazonaws.com:8080",
 });
 
 // request interceptor for adding token
