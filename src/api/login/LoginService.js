73f01c7e6b45b14e44b51bfd4af1e42b58739d90
Library User <lib-user@atss-air.home>
2023-11-28 01:25:17 -0400

    updated login api and removed auth token

--- src/api/login/LoginService.js
+++ src/api/login/LoginService.js
@@ -1,8 +1,9 @@
-import axios from "../customAxiosConfig/CustomAxiosConfig";
+// import axios from "../customAxiosConfig/CustomAxiosConfig";
+import axios from "axios";
 
 const LoginService = (username) => {
   try {
-    return axios.post(`/login`, null, {
+    return axios.post(`http://public-app-nlb-992547abe09f1ef7.elb.us-east-1.amazonaws.com/login`, null, {
       params: {
         username,
       },
