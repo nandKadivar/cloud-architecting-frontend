9e6f3459046d6089776dd6e4df89cf184c86b783
Library User <lib-user@atss-air.home>
2023-11-28 01:38:19 -0400

    updated login api and added auth token

--- src/api/login/LoginService.js
+++ src/api/login/LoginService.js
@@ -1,9 +1,8 @@
-// import axios from "../customAxiosConfig/CustomAxiosConfig";
-import axios from "axios";
+import axios from "../customAxiosConfig/CustomAxiosConfig";
 
 const LoginService = (username) => {
   try {
-    return axios.post(`http://public-app-nlb-992547abe09f1ef7.elb.us-east-1.amazonaws.com/login`, null, {
+    return axios.post(`/login`, null, {
       params: {
         username,
       },
